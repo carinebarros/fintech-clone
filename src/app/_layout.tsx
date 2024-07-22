f84685f5bafbc6acd08fe7175b21403e94d17c76
Carine <carinealzira@gmail.com>
2024-07-22 11:38:05 -0300

    feat: Add routes to the existent pages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,15 @@
+import { Ionicons } from "@expo/vector-icons";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Link, Stack, useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
+
+import Colors from "@/constants/Colors";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -18,7 +24,7 @@ export const unstable_settings = {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-const RootLayout = () => {
+const InitialLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
@@ -39,9 +45,51 @@ const RootLayout = () => {
     return null;
   }
 
-  return <RootLayoutNav />;
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="signup"
+        options={{
+          title: "",
+          headerBackTitle: "",
+          headerStyle: { backgroundColor: Colors.background },
+          headerLeft: HeaderLeft,
+          headerRight: HeaderRight,
+        }}
+      />
+      <Stack.Screen name="login" />
+    </Stack>
+  );
 };
 
-const RootLayoutNav = () => <Stack></Stack>;
+const HeaderLeft = () => {
+  const router = useRouter();
 
-export default RootLayout;
+  return (
+    <TouchableOpacity onPress={router.back}>
+      <Ionicons name="arrow-back" size={34} color={Colors.dark} />
+    </TouchableOpacity>
+  );
+};
+
+const HeaderRight = () => {
+  const router = useRouter();
+
+  return (
+    <Link href="/help" asChild>
+      <TouchableOpacity>
+        <Ionicons name="help-circle-outline" size={34} color={Colors.dark} />
+      </TouchableOpacity>
+    </Link>
+  );
+};
+
+const RootLayoutNav = () => (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <StatusBar style="light" />
+    <InitialLayout />
+  </GestureHandlerRootView>
+);
+
+export default RootLayoutNav;
